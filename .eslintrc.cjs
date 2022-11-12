e64f0d2b50dd9919452bec56cc80b5dc570d6c74
senritsu <jonas.schiegl@gmail.com>
2022-11-12 12:01:18 +0100

    :wrench: adjust linter settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,25 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
-    "plugin:storybook/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier',
+    'plugin:storybook/recommended',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
-};
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        semi: false,
+        singleQuote: true,
+      },
+    ],
+  },
+}
